d3e933125c1ad4d01308537cccae4d1c3689da8a
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-04-15 11:09:02 -0400

    Added generated subdirectory to ci

--- build-rust.sh
+++ build-rust.sh
@@ -10,6 +10,7 @@ export SDKROOT=$(xcrun -sdk macosx --show-sdk-path)
 export SWIFT_BRIDGE_OUT_DIR="$(pwd)/generated"
 # Build the project for the desired platforms:
 #cargo build --target x86_64-apple-darwin
+#cargo build --target aarch64-apple-ios-sim
 cargo build --target aarch64-apple-darwin
 cargo build --target aarch64-apple-ios
 cargo build --target x86_64-apple-ios
